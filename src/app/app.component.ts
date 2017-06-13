68a42e6a52063ff7fe67e4cce264e64d0db93815
gpzim98 <gpzim98@gmail.com>
2017-06-13 23:33:57 +0100

    Test pull request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,13 @@ import { Feedback } from './../pages/feedback/feedback';
 import { Component, ViewChild } from '@angular/core';
 import { Platform, MenuController, NavController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
-import { TabsPage } from './../pages/tabs/tabs';
 import { Storage } from '@ionic/storage';
 import { LandingPage } from './../pages/landing/landing';
 import { CadastroGerador } from './../pages/cadastro-gerador/cadastro-gerador';
 import { CadastroCatador } from './../pages/cadastro-catador/cadastro-catador';
 import { PerfilGerador } from './../pages/perfil-gerador/perfil-gerador';
 import { PerfilCatador } from './../pages/perfil-catador/perfil-catador';
-import { TutorialPage } from './../pages/tutorial/tutorial';
+
 
 @Component({
   templateUrl: 'app.html',
@@ -47,8 +46,8 @@ export class MyApp {
   openPage(page){
     switch (page) {
       case 'feedback':
-      this.nav.push(Feedback);
-      this.menuCtrl.close();
+        this.nav.push(Feedback);
+        this.menuCtrl.close();
       break;
       case 'perfil-gerador':
       this.nav.push(PerfilGerador);
