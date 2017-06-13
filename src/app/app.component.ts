6f14615af9f4aa078290e8954d04b2ff337ab19a
gpzim98 <gpzim98@gmail.com>
2017-06-13 20:18:14 +0100

    Refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,12 @@ import { Feedback } from './../pages/feedback/feedback';
 import { Component, ViewChild } from '@angular/core';
 import { Platform, MenuController, NavController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
-import { TabsPage } from './../pages/tabs/tabs';
 import { Storage } from '@ionic/storage';
 import { LandingPage } from './../pages/landing/landing';
 import { CadastroGerador } from './../pages/cadastro-gerador/cadastro-gerador';
 import { CadastroCatador } from './../pages/cadastro-catador/cadastro-catador';
 import { PerfilGerador } from './../pages/perfil-gerador/perfil-gerador';
 import { PerfilCatador } from './../pages/perfil-catador/perfil-catador';
-import { TutorialPage } from './../pages/tutorial/tutorial';
 
 @Component({
   templateUrl: 'app.html',
