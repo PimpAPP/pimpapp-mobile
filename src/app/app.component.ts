ee41ff0e42fd0f8c7cb7d53d4e317bf19d3bd4c1
Sudipta Sarkar <codersudipta@gmail.com>
2017-06-14 11:56:59 +0530

    page organized

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,7 +29,9 @@ export class MyApp {
   ) {
       platform.ready().then(() => {
         statusBar.styleDefault();
-          this.checkLandingFirstTime();
+
+          this.rootPage = TabsPage;
+          //this.checkLandingFirstTime();
       });
   }
 
