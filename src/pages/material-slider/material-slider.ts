d8927cbdddcf277b4346bd37637a6def03d6fae6
João Sizílio <jvcsizilio@gmail.com>
2017-07-10 19:04:31 -0300

    #59: Acentuação

--- src/pages/material-slider/material-slider.ts
+++ src/pages/material-slider/material-slider.ts
@@ -25,141 +25,141 @@ export class MaterialSlider {
       icon: './assets/img/material_sliderIcon_1.png',
       name: 'Papel',
       list : [
-          '- 100% reciclavel e biodegradavel;' ,        
+          '- 100% reciclável e biodegradável;' ,        
           '- Reciclamos 1/3 do produzido;',
-          '- tonelada reciclada poupa 22 arvores;',
+          '- tonelada reciclada poupa 22 árvores;', //
           '- Evite molhar, rasgar ou amassar;',
-          '- Nao misture papel engordurado;',
-          '- Lave embalagens longa vida no enxague da louca.'
+          '- Não misture papel engordurado;',
+          '- Lave embalagens longa vida no enxágue da louça.'
       ],
-      desc: 'Combine com o catador qual e a remuneracao adequada para cada coleta.',
+      desc: 'Combine com o catador qual é a remuneração adequada para cada coleta.',
       subdesc : '' 
     },
     {
       icon: './assets/img/material_sliderIcon_2.png',
-      name: 'Aluminio',
+      name: 'Alumínio',
       list : [
-          '- Em 60 dias, 1 late e produzida, comprada, coletada, reciclada e volta as prateleiras;' ,        
-          '- Reciclamos 98.4% das latinhas do Brasil. E apenas 33% dos outros itens de aluminio;',
+          '- Em 60 dias, 1 lata é produzida, comprada, coletada, reciclada e volta as prateleiras;' ,        
+          '- Reciclamos 98.4% das latinhas do Brasil. E apenas 33% dos outros itens de alumínio;',
           '- Reciclar gasta 95% menos energia do que produzir.',
       ],
-      desc: 'Combine com o catador qual e a remuneracao adequada para cada coleta.',
+      desc: 'Combine com o catador qual é a remuneração adequada para cada coleta.',
       subdesc : '' 
     },
     {
       icon: './assets/img/material_sliderIcon_3.png',
-      name: 'Plasticos',
+      name: 'Plásticos',
       list : [
-          '- O Brasil so recicla 20% da producao;' ,        
-          '- 8 bilhoes de kg/ano poluem os oceanos;',
-          '- Recicle sacolas, salgadinhos, saches, canudos, isopor e todos os outros plasticos;',
-          '- Limpe restos de alimento com ague do enxague da louca ou guardanapo usado.'
+          '- O Brasil só recicla 20% da produção;' ,        
+          '- 8 bilhões de kg/ano poluem os oceanos;',
+          '- Recicle sacolas, salgadinhos, sachês, canudos, isopor e todos os outros plásticos;',
+          '- Limpe restos de alimento com água do enxague da louça ou guardanapo usado.'
       ],
-      desc: 'Combine com o catador qual e a remuneracao adequada para cada coleta.',
+      desc: 'Combine com o catador qual é a remuneração adequada para cada coleta.',
       subdesc : '' 
     },
     {
       icon: './assets/img/material_sliderIcon_4.png',
       name: 'Vidros',
       list : [
-          '- 100% reciclavel, e dura para sempre;' ,        
-          '- Preco baixo, poucos lugares reciclam;',
-          '- Vidro quebrado e ainda pior;',
-          '- Maior numero de acidentes com catadores;',
-          '- Lampadas, TV, termicas e espelhos precisam de atencao especial, combine;',
-          '- Maior numero de acidentes com catadores.'
+          '- 100% reciclável, e dura para sempre;' ,        
+          '- Preço baixo, poucos lugares reciclam;',
+          '- Vidro quebrado é ainda pior;',
+          '- Maior número de acidentes com catadores;',
+          '- Lâmpadas, TV, térmicas e espelhos precisam de atenção especial, combine;',
+          '- Maior número de acidentes com catadores.'
       ],
-      desc: 'Combine com o catador qual e a remuneracao adequada para cada coleta.',
+      desc: 'Combine com o catador qual é a remuneração adequada para cada coleta.',
       subdesc : '' 
     },
     {
       icon: './assets/img/material_sliderIcon_5.png',
-      name: 'Oleo',
+      name: 'Óleo',
       list : [
-          '- 1L no ralo = 25 mil L de agua poluida;' ,        
+          '- 1L no ralo = 25 mil L de água poluída;' ,        
           '- Nunca descarte na pia ou no vaso;',
-          '- Depois de usado e esfriado, o oleo deveser coado num garrafa PET com tampa;',
-          '- Peca ao catador para levar a um ponto de coleta ou cooperativa que produz sabao.'
+          '- Depois de usado e esfriado, o óleo deve ser coado numa garrafa PET com tampa;',
+          '- Peça ao catador para levar a um ponto de coleta ou cooperativa que produz sabão.'
       ],
-      desc: 'Combine com o catador qual e a remuneracao adequada para cada coleta.',
+      desc: 'Combine com o catador qual é a remuneração adequada para cada coleta.',
       subdesc : '' 
     },
     {
       icon: './assets/img/material_sliderIcon_6.png',
-      name: 'Residuos Misturados',
+      name: 'Resíduos Misturados',
       list : [
-          '- E o pior material para os catadores, seu valor e muito baixo;' ,        
-          '- O Brasil perde R$ 8 bilhoes/ano enterrando reciclaveis misturados;',
-          '- Remedios e suas embalagens laminadas devem ser descartados nas farmacias.'
+          '- É o pior material para os catadores, seu valor é muito baixo;' ,        
+          '- O Brasil perde R$ 8 bilhões/ano enterrando recicláveis misturados;',
+          '- Remédios e suas embalagens laminadas devem ser descartados nas farmácias.'
       ],
-      desc: 'Combine com o catador qual e a remuneracao adequada para cada coleta.',
+      desc: 'Combine com o catador qual é a remuneração adequada para cada coleta.',
       subdesc : '' 
     },
     {
       icon: './assets/img/material_sliderIcon_7.png',
       name: 'Metal',
       list : [
-          '- Aco e o material mais reciclado do mundo: e 100% reciclavel, infinitas vezes;' ,        
-          '- 1/4 do aco no Brasil utiliza materia reciclada;',
-          '- Metais reciclados estao em latas de alimentos, ferramentas, arames, vigas, automoveis e utensilios domesticos.'
+          '- Aço é o material mais reciclado do mundo: é 100% reciclável, infinitas vezes;' ,        
+          '- 1/4 do aço no Brasil utiliza matéria reciclada;',
+          '- Metais reciclados estão em latas de alimentos, ferramentas, arames, vigas, automóveis e utensílios domésticos.'
       ],
-      desc: 'Combine com o catador qual e a remuneracao adequada para cada coleta.',
+      desc: 'Combine com o catador qual é a remuneração adequada para cada coleta.',
       subdesc : '' 
     },
     {
       icon: './assets/img/material_sliderIcon_8.png',
-      name: 'Eletronicos',
+      name: 'Eletrônicos',
       list : [
-          '- 98% dos componentes de um eletronico podem ser reciclados;' ,        
-          '- Tem valor mais alto, mas so 15.5% e reciclado: falta infraestrutura no Brasil;',
-          '- Sao altamente contaminantes : busque a destinacao correta.'
+          '- 98% dos componentes de um eletrônico podem ser reciclados;' ,        
+          '- Tem valor mais alto, mas só 15.5% é reciclado: falta infraestrutura no Brasil;',
+          '- São altamente contaminantes : busque a destinação correta.'
       ],
-      desc: 'Combine com o catador qual e a remuneracao adequada para cada coleta.',
+      desc: 'Combine com o catador qual é a remuneração adequada para cada coleta.',
       subdesc : '' 
     },
     {
       icon: './assets/img/material_sliderIcon_9.png',
       name: 'Baterias',
       list : [
-          '- Sao altamente contaminantes;' ,        
-          '- Estao nos celulares, telefones sem fio, maquinas fotograficas, filmadoras, laptops, brinquedos e relogios.',
-          '- Nao podem ser misturadas : separe e avise o catador antes da coleta.'
+          '- São altamente contaminantes;' ,        
+          '- Estão nos celulares, telefones sem fio, máquinas fotográficas, filmadoras, laptops, brinquedos e relógios.',
+          '- Não podem ser misturadas : separe e avise o catador antes da coleta.'
       ],
-      desc: 'Combine com o catador qual e a remuneracao adequada para cada coleta.',
+      desc: 'Combine com o catador qual é a remuneração adequada para cada coleta.',
       subdesc : '' 
     },
     {
       icon: './assets/img/material_sliderIcon_10.png',
-      name: 'Moveis',
+      name: 'Móveis',
       list : [
-          '- Cheque o tamanho da carroca;' ,        
-          '- Combine com o catador o ponto de descarte dos moveis;',
-          '- Ideal: reciclagem de moveis;',
-          '- Garanta que o movel nao terminara em um terreno baldio ou na rua.'
+          '- Cheque o tamanho da carroça;' ,        
+          '- Combine com o catador o ponto de descarte dos móveis;',
+          '- Ideal: reciclagem de móveis;',
+          '- Garanta que o móvel não terminará em um terreno baldio ou na rua.'
       ],
-      desc: 'Combine com o catador qual e a remuneracao adequada para cada coleta.',
+      desc: 'Combine com o catador qual é a remuneração adequada para cada coleta.',
       subdesc : ''
     },
     {
       icon: './assets/img/material_sliderIcon_11.png',
       name: 'Entulho',
       list : [
-          '- Pode-se aproveitar 95% do material e sue reciclagem e obrigatoria;' ,        
+          '- Pode-se aproveitar 95% do material e sua reciclagem é obrigatória;' ,        
           '- Reutilizado em ruas e estradas;',
-          '- Garanta que seu entulho nao acabara em um terreno baldio: cheque com sua Prefeitura o local adequado.'
+          '- Garanta que seu entulho não acabará em um terreno baldio: cheque com sua Prefeitura o local adequado.'
       ],
-      desc: 'Combine com o catador qual e a remuneracao adequada para cada coleta.',
+      desc: 'Combine com o catador qual é a remuneração adequada para cada coleta.',
       subdesc : ''
     },
     {
       icon: './assets/img/material_sliderIcon_12.png',
       name: 'Outros',
       list : [
-          '- Podas de arvore podem ser compostadas e produzir energia;' ,        
-          '- Em eventos e festas, organize a separacao dos reciclaveis secos em sacos grandes;',
-          '- Avalie e combine com o catador a coleta dos residuos nao listados neste aplicativo.'
+          '- Podas de árvore podem ser compostadas e produzir energia;' ,        
+          '- Em eventos e festas, organize a separação dos recicláveis secos em sacos grandes;',
+          '- Avalie e combine com o catador a coleta dos resíduos não listados neste aplicativo.'
       ],
-      desc: 'Combine com o catador qual e a remuneracao adequada para cada coleta.',
+      desc: 'Combine com o catador qual é a remuneração adequada para cada coleta.',
       subdesc : ''
     },
     {
@@ -168,8 +168,8 @@ export class MaterialSlider {
       list : [
           /*'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tortor eros, tempor sed fringilla id, iaculis et orci. Nullam lorem orci, iaculis at odio in, porta pulvinar risus.'*/
       ],
-      desc: 'Combine com o catador qual e a remuneracao adequada para cada coleta.',
-      subdesc : 'Residuos misturados tem um valor medio de venda de'
+      desc: 'Combine com o catador qual é a remuneração adequada para cada coleta.',
+      subdesc : 'Resíduos misturados tem um valor médio de venda de'
     }
   ];
 
